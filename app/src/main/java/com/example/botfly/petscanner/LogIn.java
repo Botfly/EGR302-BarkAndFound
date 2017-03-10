aebbe1687b80e93eede3a9c851c7238cd2d894b4
Botfly <alex@thebarks.com>
2017-03-10 12:40:34 -0800

    1st (Alex) - Initial Commit

---
+++ app/src/main/java/com/example/botfly/petscanner/LogIn.java
@@ -0,0 +1,23 @@
+package com.example.botfly.petscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+    public void loginClicked(View view) {
+        System.out.println("kijhdfskjh");
+    }
+
+    //TextView textViewSignIn = (TextView) findViewById(R.id.textView_signUp);
+    //textViewSignIn.setMovementMethod(LinkMovementMethod.getInstance());
+}
