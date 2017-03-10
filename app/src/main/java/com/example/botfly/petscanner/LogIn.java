d9e160c9206d85415aa7e41fa071df61ad95f760
Botfly <alex@thebarks.com>
2017-03-10 14:13:47 -0800

    2nd (Alex) - barely any changes

--- app/src/main/java/com/example/botfly/petscanner/LogIn.java
+++ app/src/main/java/com/example/botfly/petscanner/LogIn.java
@@ -1,9 +1,11 @@
 package com.example.botfly.petscanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
@@ -15,8 +17,18 @@ public class LogIn extends AppCompatActivity {
     }
 
     public void loginClicked(View view) {
+        setContentView(R.id.activity_log_in);
         System.out.println("kijhdfskjh");
-    }//j
+    }
+
+//    Button btn = (Button)findViewById(R.id.Button_LogIn);
+//
+//    btn.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            startActivity(new Intent(LogIn.this, SignUpScreen.class));
+//        }
+//    });
 
     //TextView textViewSignIn = (TextView) findViewById(R.id.textView_signUp);
     //textViewSignIn.setMovementMethod(LinkMovementMethod.getInstance());
