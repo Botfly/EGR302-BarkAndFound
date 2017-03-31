1c92db5bed01ea6714310bc1debea4957bcc1afa
Botfly <alex@thebarks.com>
2017-03-30 21:20:28 -0700

    5th (Alex) - More buttons working

--- app/src/main/java/com/example/botfly/petscanner/Register.java
+++ app/src/main/java/com/example/botfly/petscanner/Register.java
@@ -2,6 +2,7 @@ package com.example.botfly.petscanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -10,4 +11,5 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
 }
