a901b7f49bd7c62303ff6333e4c178ddb497611b
anthonyg900 <anthony.gomez@calbaptist.edu>
2017-04-03 18:57:39 -0700

    Anthony - Register Hash-Map

--- app/src/main/java/com/example/botfly/petscanner/Register.java
+++ app/src/main/java/com/example/botfly/petscanner/Register.java
@@ -1,13 +1,19 @@
 package com.example.botfly.petscanner;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
 
@@ -30,8 +36,10 @@ public class Register extends AppCompatActivity {
         //Creates button to update database in real-time
         register = (Button) findViewById(R.id.Register);
 
-        //creates link reference to database
+        //creates link reference to root of database
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        //Creates link reference to root of a specific user
+        //mDatabase = FirebaseDatabase.getInstance().getReference().getchild("User_01");
 
         //Creates local variable for user input on each field
         firstName = (EditText) findViewById(R.id.first_name);
@@ -44,17 +52,30 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                // 1 - create a child in root of the database
+                // 2- Assign value to the child
+
                 String name = firstName.getText().toString().trim();
                 String lName = lastName.getText().toString().trim();
 
-                // 1 - create a child in root of the database
-                mDatabase.child("First Name").setValue(name);
-                mDatabase.child("Last Name").setValue(lName);
-                mDatabase.child("Email Address");
-                mDatabase.child("Password");
-                mDatabase.child("Confirm Password");
+                //Create Hash-map to make object with all the fields
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Name", name);
+                dataMap.put("Last Name", lName);
 
-                // 2- Assign value to the child
+                //Push Hash Object to root of Database
+                //The on-complete listener makes sure the information was pushed
+                //successfully to the database.
+                mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "Stored..", Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(Register.this, "", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
             }
         });
@@ -62,3 +83,18 @@ public class Register extends AppCompatActivity {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
