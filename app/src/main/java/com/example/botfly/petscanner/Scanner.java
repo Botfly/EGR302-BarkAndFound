c1a5d0d1c5bb5ecbee391c0c12962718790a611e
Taylor Moe <t.moe55@frontier.com>
2017-04-25 16:38:31 -0700

    Taylor - updated scanner

--- app/src/main/java/com/example/botfly/petscanner/Scanner.java
+++ app/src/main/java/com/example/botfly/petscanner/Scanner.java
@@ -1,11 +1,8 @@
 package com.example.botfly.petscanner;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import com.google.zxing.Result;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -13,11 +10,17 @@ import android.app.Activity;
 import android.util.Log;
 
 
-public class Scanner extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+public class Scanner extends Activity implements ZXingScannerView.ResultHandler {
     private ZXingScannerView mScannerView;
 
 
-    public void scannerQRscanClicked(View v){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+
+    public void scannerQRscanClicked(View view){
         mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
         mScannerView.setResultHandler(this);
@@ -42,24 +45,6 @@ public class Scanner extends AppCompatActivity implements ZXingScannerView.Resul
 
         //resume scanning
         //mScannerView.resumeCameraPreview(this);
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_scanner);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     public void scannerLogInButton(View view) {
